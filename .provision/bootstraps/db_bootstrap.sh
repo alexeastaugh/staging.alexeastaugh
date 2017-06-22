e89537dbbb613677df31f35836616cee5955d78f
Alex Eastaugh <alex.eastaugh@visiondirect.co.uk>
2017-06-22 15:01:18 +0100

    database changes

--- .provision/bootstraps/db_bootstrap.sh
+++ .provision/bootstraps/db_bootstrap.sh
@@ -4,17 +4,23 @@
 if [ ! -f /var/log/firsttime ]; then
     touch /var/log/firsttime
     sudo apt-get update
-    
+
     # Set root password
     sudo apt-get install debconf-utils -y > /dev/null 2>&1
     sudo debconf-set-selections <<< "mysql-server mysql-server/root_password password password"
     sudo debconf-set-selections <<< "mysql-server mysql-server/root_password_again password password"
-    
+
     # Install mysql
     sudo apt-get install mysql-server -y > /dev/null 2>&1
 
     # Allow remote access
     mysql -u root -ppassword -e "GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY 'password' WITH GRANT OPTION;"
+
+    # Create ghost user
+    CREATE USER 'ghost'@'192.168.50.20' IDENTIFIED BY 'ghost';
+
+    # Grant permissions to ghost database
+    GRANT ALL PRIVILEGES ON `ghost`.* TO 'ghost'@'192.168.50.20';
 
     # Flush privileges
     mysql -u root -ppassword -e "FLUSH PRIVILEGES;"
